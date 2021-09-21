3bfabb43ccf4f5b6725cb62f91ea7eeafae29956
inenought <joao.vitor328@gmail.com>
2021-09-21 18:17:53 -0300

    UNITY ADS

--- Assets/Scripts/UI_Buttons.cs
+++ Assets/Scripts/UI_Buttons.cs
@@ -6,23 +6,19 @@ public class UI_Buttons : MonoBehaviour
 {
     public void ExitGame()
     {
+        //call save functions
         Application.Quit();
     }
     public void FreeRoad()
     {
-
+        //this method do not exist until... today
     }
     public void Adventure()
     {
-
+        GameManager.gt = GameManager.GameStates.playing;
     }
     public void BacktoMenu()
     {
-
+        GameManager.gt = GameManager.GameStates.menu;
     }
-    public void CloseAd()
-    {
-
-    }
-
 }
